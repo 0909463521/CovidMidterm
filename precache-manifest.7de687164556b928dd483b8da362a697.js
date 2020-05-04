c2b63486a11ba86e8085728158557ce256fad533
1653005 <1653005@student.hcmus.edu.vn>
2020-05-04 18:05:49 +0700

    Updates

---
+++ precache-manifest.7de687164556b928dd483b8da362a697.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d367929cf0aefbaf0d259f776c76df9",
+    "url": "/CovidMidterm/index.html"
+  },
+  {
+    "revision": "554b1d09e0b58b751f81",
+    "url": "/CovidMidterm/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9fc1b7ac64d3f507ddd2",
+    "url": "/CovidMidterm/static/css/main.d21d54fa.chunk.css"
+  },
+  {
+    "revision": "554b1d09e0b58b751f81",
+    "url": "/CovidMidterm/static/js/2.dd5beb63.chunk.js"
+  },
+  {
+    "revision": "888e83268afc9eb9ff457eaa1aee4f1f",
+    "url": "/CovidMidterm/static/js/2.dd5beb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc1b7ac64d3f507ddd2",
+    "url": "/CovidMidterm/static/js/main.8ecd3cff.chunk.js"
+  },
+  {
+    "revision": "355ac688d6b7cc31c0eb",
+    "url": "/CovidMidterm/static/js/runtime-main.80b20e35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CovidMidterm/static/media/logo.5d5d9eef.svg"
+  }
+]);
